edc30dea5718e4f39ba8fca9bbbdd7caf6838084
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-10-25 16:39:58 -0700

    props between CL.js and CC.js set up, pulling effectively

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,2 +1,16 @@
 import React from 'react';
 
+const CharacterCard = (props) => {
+    return (
+        <div>
+            <div>
+                <img></img>
+                <div>
+                    <h2>{props.attribute.name}</h2>
+                    <p>Birth Year: {props.attribute.birth_year}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default CharacterCard;
